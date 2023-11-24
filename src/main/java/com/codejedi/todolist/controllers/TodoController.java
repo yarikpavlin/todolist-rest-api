3446207310d353bbf2d3732edcf833f69bea1009
Yaroslav Pavlynkyi <yarbezl@gmail.com>
2023-11-24 23:05:00 +0200

    feat: add validation of request body for post route
    - add handleValidationExceptions route

--- src/main/java/com/codejedi/todolist/controllers/TodoController.java
+++ src/main/java/com/codejedi/todolist/controllers/TodoController.java
@@ -3,15 +3,19 @@ package com.codejedi.todolist.controllers;
 import com.codejedi.todolist.exceptions.TodoNotFoundException;
 import com.codejedi.todolist.models.Todo;
 import com.codejedi.todolist.services.TodoService;
+import jakarta.validation.Valid;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(
@@ -34,7 +38,7 @@ public class TodoController {
     }
 
     @PostMapping
-    public ResponseEntity<Todo> saveTodo(@RequestBody Todo newTodo) {
+    public ResponseEntity<Todo> saveTodo(@RequestBody @Valid Todo newTodo) {
         return new ResponseEntity<>(todoService.create(newTodo), HttpStatus.CREATED);
     }
 
@@ -52,5 +56,18 @@ public class TodoController {
     @ExceptionHandler(value = TodoNotFoundException.class)
     public ResponseEntity<String> handleToDoNotFoundException(TodoNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
